cddc720232a6eac229562b2a73db7bb00be27d72
narendraprasathnp7@gmail.com <narendraprasathnp7@gmail.com>
2021-12-18 23:03:12 -0800

    win

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import CardName from './Card';
+import Thumbnail from './Thumbnail';
+
+
+function App() {
+  return (
+    <div className='container-fluid'>
+      <div className='row'>
+
+      
+     <Thumbnail imgUrl="https://randomwordgenerator.com/img/picture-generator/54e2d2464e57aa14f1dc8460962e33791c3ad6e04e507440752972d3934fc0_640.jpg"></Thumbnail>
+     <Thumbnail imgUrl="https://randomwordgenerator.com/img/picture-generator/53e3d745425ba814f1dc8460962e33791c3ad6e04e507440762e7ad3914cc4_640.jpg"></Thumbnail>
+     <Thumbnail imgUrl="https://randomwordgenerator.com/img/picture-generator/57e8d5444e55aa14f1dc8460962e33791c3ad6e04e507749772772d5914dc1_640.jpg"></Thumbnail>
+     <Thumbnail imgUrl="https://randomwordgenerator.com/img/picture-generator/52e7dc414851af14f1dc8460962e33791c3ad6e04e5077497c2a7cd49349cc_640.jpg"></Thumbnail>
+     <Thumbnail imgUrl="https://randomwordgenerator.com/img/picture-generator/54e2d2464e57aa14f1dc8460962e33791c3ad6e04e507440752972d3934fc0_640.jpg"></Thumbnail>
+     <Thumbnail imgUrl="https://randomwordgenerator.com/img/picture-generator/53e3d745425ba814f1dc8460962e33791c3ad6e04e507440762e7ad3914cc4_640.jpg"></Thumbnail>
+     <Thumbnail imgUrl="https://randomwordgenerator.com/img/picture-generator/57e8d5444e55aa14f1dc8460962e33791c3ad6e04e507749772772d5914dc1_640.jpg"></Thumbnail>
+     <Thumbnail imgUrl="https://randomwordgenerator.com/img/picture-generator/52e7dc414851af14f1dc8460962e33791c3ad6e04e5077497c2a7cd49349cc_640.jpg"></Thumbnail>
+     
+
+
+</div>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,7 @@
+export default function CardName(props){
+    return <div>
+        <h1>
+            hello {props.name}
+        </h1>
+    </div>
+}
