44391e1992623a07163643b55b0c4c99118d16a6
narendraprasathnp7@gmail.com <narendraprasathnp7@gmail.com>
2021-12-18 23:09:30 -0800

    change

--- src/Thumbnail.js
+++ src/Thumbnail.js
@@ -1,11 +1,11 @@
 export default function Thumbnail(props){
     return<div className="col-lg-3">
      <div class="card">
-    <img class="card-img-top" src={props.imgUrl} alt="Card image cap"/>
+    <img class="card-img-top" src={props.imgUrl} alt="Card cap"/>
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <a href="/#" class="btn btn-primary">Go somewhere</a>
     </div>
   </div>
   </div>
